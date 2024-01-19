ce77e522c502b8f5778d7885c157dc1b3e0fb937
mbintangw <mbintangw@gmail.com>
2024-01-19 13:51:44 +0700

    Define todos state and show them in the browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Finish Progate React Course',
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'Have lunch with guru Domba',
+      completed: false
+    },
+    {
+      id: 3,
+      title: 'Study React with Ninja Ken',
+      completed: false
+    },
+  ])
+
+  console.log(todos)
+
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div>
+      <h1>My Todo List</h1>
+      {todos.map((todo) => {
+        return <p key={todo.id}>{todo.title}</p>
+      })}
     </div>
   )
 }
