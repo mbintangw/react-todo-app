89d2b4251b0b627b2067e59c7068d0e8f6226bba
mbintangw <mbintangw@gmail.com>
2024-01-19 13:33:59 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  )
+}
+
+export default App
